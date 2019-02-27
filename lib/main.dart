43fbc6cd3277c8e30d4f836227dd8c2296656981
chiragsastry1996 <chiragsastry@gmail.com>
2019-02-27 11:10:52 +0530

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         
-        primarySwatch: Colors.yellow
+        primarySwatch: Colors.pink
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
