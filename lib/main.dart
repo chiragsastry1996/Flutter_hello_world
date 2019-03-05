1c1923e59a4c2b9a212a9e7e38eba46342fab8b6
chiragsastry1996 <chiragsastry@gmail.com>
2019-03-05 09:14:10 +0530

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,14 +10,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("EasyList"),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text("Food Paradise")
-            ],
-          ),
-        ),
+        body: ProductManager(),
       ),
     );
   }
