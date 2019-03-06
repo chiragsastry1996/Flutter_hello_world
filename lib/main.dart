e97fdc5f113a56b8d52b084b7df63a0759c7610a
chiragsastry1996 <chiragsastry@gmail.com>
2019-03-06 18:07:28 +0530

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'status.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: SquareCard(),
     );
@@ -14,57 +14,58 @@ class MyApp extends StatelessWidget {
 class SquareCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var item_list = ["Status", "Chat", "Phone", "Team"];
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-            appBar: AppBar(
-              title: Text("Card Example"),
-            ),
-            body: Center(
-              child: Container(
-                      alignment: Alignment.center,
-                      height: MediaQuery.of(context).size.width*0.85,
-                      width: MediaQuery.of(context).size.width*0.85,
-                      child: Card(
-                        child: Row(
-//                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Column(
-//                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Card(
-                                  child: Card(
-                                    child: Text("Button 1"),
-                                  ),
-                                ),
-                                Card(
-                                  child: Card(
-                                    child: Text("Button 2"),
-                                  ),
-                                )
-                              ],
-                            ),
-                            Column(
-//                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Card(
-                                  child: Card(
-                                    child: Text("Button 3"),
-                                  ),
-                                ),
-                                Card(
-                                  child: Card(
-                                    child: Text("Button 4"),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                      )
-              ),
-            )
+        appBar: AppBar(
+          title: Text("DBS"),
+        ),
+        body: Center(
+          child: Container(
+              alignment: Alignment.center,
+              height: MediaQuery.of(context).size.width * 0.85,
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    children: _getTiles(item_list),
+                  ),
+                ),
+              )),
+        ),
       ),
     );
   }
+}
+
+// Function to be called on click
+void _onTileClicked(int index) {
+  debugPrint("You tapped on item $index");
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => status()),
+  );
+}
+
+// Get grid tiles
+List<Widget> _getTiles(List<String> navList) {
+  final List<Widget> tiles = <Widget>[];
+  for (int i = 0; i < navList.length; i++) {
+    tiles.add(new GridTile(
+      child: new Card(
+          color: const Color(0xff2980b9),
+          margin: const EdgeInsets.all(10.0),
+          child: new InkResponse(
+            enableFeedback: true,
+            child: Center(
+              child: new Text(navList[i]),
+            ),
+            onTap: () => _onTileClicked(i),
+          )),
+    ));
+  }
+  return tiles;
 }
